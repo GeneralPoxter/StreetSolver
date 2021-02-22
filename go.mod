2aabb4d62cc7949764b1dffb208c3a0dbbcbbc93
GeneralPoxter <generalpoxter@gmail.com>
2021-02-22 10:15:23 -0500

    yay

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GeneralPoxter/StreetSolver
+
+go 1.16
---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/map", mapHandler)
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func mapHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/map" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+	}
+
+	res, err := http.Get("https://maps.googleapis.com/maps/api/js?key=AIzaSyBIwzALxUPNbatRBj3Xi1Uhp0fFzwWNBkE&callback=initialize&libraries=&v=weekly")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Fprintf(w, string(body))
+}
