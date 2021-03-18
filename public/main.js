38c2701589007bf363e7fb522a95710f85bde531
GeneralPoxter <generalpoxter@gmail.com>
2021-03-18 12:16:58 -0400

    test my new token

--- public/main.js
+++ public/main.js
@@ -7,44 +7,46 @@ let panorama;
 
 window.initMap = function() {
 	const sv = new google.maps.StreetViewService();
-	const randomLoc = { lat: rangeRandom(-80, 80), lng: rangeRandom(-180, 180)};
+	const randomLoc = { lat: rangeRandom(-80, 80), lng: rangeRandom(-180, 180) };
 	/** For US-only:
 	const randomLoc = { lat: rangeRandom(30, 50), lng: rangeRandom(-125, -65) };
 	*/
-	
+
 	sv.getPanorama({
-		location: randomLoc,
-		radius: 100000,
-		source: google.maps.StreetViewSource.OUTDOOR
-	}, function(data, status) {
-		if (status === 'OK') {
-			map = new google.maps.Map(document.getElementById('guessingMap'), {
-				center: {
-					lat: 0,
-					lng: 0
-				},
-				zoom: 1,
-				streetViewControl: false,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			});
-			panorama = new google.maps.StreetViewPanorama(document.getElementById('streetView'));
-			panorama.setOptions({
-				addressControl: false,
-				showRoadLabels: false
-			});
-		
-			panorama.setPano(data.location.pano);
-			panorama.setPov({
-				heading: 270,
-				pitch: 0
-			});
-			panorama.setVisible(true);
-		} else {
-			console.log("Street View not found.");
-			window.initMap();
+			location: randomLoc,
+			radius: 100000,
+			source: google.maps.StreetViewSource.OUTDOOR
+		},
+		function(data, status) {
+			if (status === 'OK') {
+				map = new google.maps.Map(document.getElementById('guessingMap'), {
+					center: {
+						lat: 0,
+						lng: 0
+					},
+					zoom: 1,
+					streetViewControl: false,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				});
+
+				panorama = new google.maps.StreetViewPanorama(document.getElementById('streetView'));
+				panorama.setOptions({
+					addressControl: false,
+					showRoadLabels: false
+				});
+
+				panorama.setPano(data.location.pano);
+				panorama.setPov({
+					heading: 270,
+					pitch: 0
+				});
+				panorama.setVisible(true);
+			} else {
+				console.log('Street View not found.');
+				window.initMap();
+			}
 		}
-	});
-	
+	);
 };
 
 function rangeRandom(min, max) {
