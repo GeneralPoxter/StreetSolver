e42d5a3035b9148ce1cdc82168af6b63568ab4cc
unknown <naha71012@gmail.com>
2021-03-22 09:49:00 -0400

    Pushing for Merging

--- public/main.js
+++ public/main.js
@@ -6,89 +6,96 @@ let map;
 let panorama;
 
 var markerLat, markerLong;
+var randomLoc;
 
 window.initMap = function() {
 	const sv = new google.maps.StreetViewService();
-	const randomLoc = { lat: rangeRandom(-80, 80), lng: rangeRandom(-180, 180) };
 	/** For US-only:
 	const randomLoc = { lat: rangeRandom(30, 50), lng: rangeRandom(-125, -65) };
 	*/
-
+	randomLoc = { lat: rangeRandom(-80, 80), lng: rangeRandom(-180, 180) };
 	sv.getPanorama({
 			location: randomLoc,
 			radius: 100000,
 			source: google.maps.StreetViewSource.OUTDOOR
 		},
-		function(data, status) {
-			if (status === 'OK') {
-				guessingMap = new google.maps.Map(document.getElementById('guessingMap'), {
-					center: {
-						lat: 0,
-						lng: 0
-					},
-					zoom: 1,
-					streetViewControl: false,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-				});
-
-				var marker = new google.maps.Marker({
-					map: guessingMap,
-					position: {lat: 0, lng: 0},
-					draggable: true,
-					title: 'Guessing Marker'
-				});
-				
-				google.maps.event.addListener(marker, 'dragend', function() {
-					updateMarker();
-				});
-
-				guessingMap.addListener('click', (mapsMouseEvent) => {
-					marker.setPosition(mapsMouseEvent.latLng);
-					updateMarker();
-				});
-
-				function updateMarker(){
-					markerLat = marker.getPosition().lat();
-					markerLong = marker.getPosition().lng();
-					document.getElementById("marker").innerHTML = "Marker At: "+markerLat+","+markerLong;
-					console.log(markerLat+","+markerLong);
-				}
-
-				panorama = new google.maps.StreetViewPanorama(document.getElementById('streetView'));
-				panorama.setOptions({
-					addressControl: false,
-					showRoadLabels: false
-				});
-
-				panorama.setPano(data.location.pano);
-				panorama.setPov({
-					heading: 270,
-					pitch: 0
-				});
-				panorama.setVisible(true);
-			} else {
-				console.log('Street View not found.');
-				window.initMap();
-			}
-		}
+		initGame
 	);
 };
+
+function initGame(data, status) {
+	if (status === 'OK') {
+		guessingMap = new google.maps.Map(document.getElementById('guessingMap'), {
+			center: {
+				lat: 0,
+				lng: 0
+			},
+			zoom: 1,
+			streetViewControl: false,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
+
+		var marker = new google.maps.Marker({
+			map: guessingMap,
+			position: {lat: 0, lng: 0},
+			draggable: true,
+			title: 'Guessing Marker'
+		});
+		
+		google.maps.event.addListener(marker, 'dragend', function() {
+			updateMarker();
+		});
+
+		guessingMap.addListener('click', (mapsMouseEvent) => {
+			marker.setPosition(mapsMouseEvent.latLng);
+			updateMarker();
+		});
+
+		function updateMarker(){
+			markerLat = marker.getPosition().lat();
+			markerLong = marker.getPosition().lng();
+			document.getElementById("marker").innerHTML = "Marker At: "+markerLat+","+markerLong;
+			console.log(markerLat+","+markerLong);
+		}
+
+		document.getElementsById('guess').click(function() {
+			console.log(calcScore());
+		})
+
+		panorama = new google.maps.StreetViewPanorama(document.getElementById('streetView'));
+		panorama.setOptions({
+			addressControl: false,
+			showRoadLabels: false
+		});
+
+		panorama.setPano(data.location.pano);
+		panorama.setPov({
+			heading: 270,
+			pitch: 0
+		});
+		panorama.setVisible(true);
+	} else {
+		console.log('Street View not found.');
+		window.initMap();
+	}
+}
 
 function rangeRandom(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
-/*** 
-function() {
+function calcScore(){
+	const latRad1 = markerLat * Math.PI/180;
+	const latRad2 = randomLoc.lat * Math.PI/180;
+	const deltaLat = (randomLoc.lat-markerLat) * Math.PI/180;
+	const deltaLng = (randomLoc.lng-markerLong) * Math.PI/180;
+	const a = Math.pow(Math.sin(deltaLat/2), 2) + Math.cos(latRad1) * Math.cos(latRad2) * Math.pow(Math.sin(deltaLng),2);
+	const c = 2 * Math.atan2(Math.sqrt(a), Marth.sqrt(1-a));
 
-	streetview = new google.maps.StreetViewPanorama(document.getElementById('streetView'), {
-		position: { 
-			lat: 48.8584,
-			lng: 2.296
-		}
-	});
-
-};
-*/
+	const distance = 6371e3 * c; //in meters
+	
+	var score = 5000*Math.pow(e,(-distance/2000))
+	return score;
+}
 
 document.head.appendChild(script);
