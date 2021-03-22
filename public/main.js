7d498ae479b4b863e3c511ac896fc744f72899fa
GeneralPoxter <generalpoxter@gmail.com>
2021-03-22 09:44:02 -0400

    nobody likes css

--- public/main.js
+++ public/main.js
@@ -14,65 +14,72 @@ window.initMap = function() {
 	const randomLoc = { lat: rangeRandom(30, 50), lng: rangeRandom(-125, -65) };
 	*/
 
-	sv.getPanorama({
+	sv.getPanorama(
+		{
 			location: randomLoc,
 			radius: 100000,
 			source: google.maps.StreetViewSource.OUTDOOR
 		},
-		function(data, status) {
-			if (status === 'OK') {
-				guessingMap = new google.maps.Map(document.getElementById('guessingMap'), {
-					center: {
-						lat: 0,
-						lng: 0
-					},
-					zoom: 1,
-					streetViewControl: false,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-				});
-
-				var marker = new google.maps.Marker({
-					map: guessingMap,
-					position: {lat: 0, lng: 0},
-					draggable: true,
-					title: 'Guessing Marker'
-				});
-				
-				google.maps.event.addListener(marker, 'dragend', function() {
-					updateMarker();
-				});
-
-				guessingMap.addListener('click', (mapsMouseEvent) => {
-					marker.setPosition(mapsMouseEvent.latLng);
-					updateMarker();
-				});
-
-				function updateMarker(){
-					markerLat = marker.getPosition().lat();
-					markerLong = marker.getPosition().lng();
-					document.getElementById("marker").innerHTML = "Marker At: "+markerLat+","+markerLong;
-					console.log(markerLat+","+markerLong);
-				}
-
-				panorama = new google.maps.StreetViewPanorama(document.getElementById('streetView'));
-				panorama.setOptions({
-					addressControl: false,
-					showRoadLabels: false
-				});
-
-				panorama.setPano(data.location.pano);
-				panorama.setPov({
-					heading: 270,
-					pitch: 0
-				});
-				panorama.setVisible(true);
-			} else {
-				console.log('Street View not found.');
-				window.initMap();
-			}
-		}
+		initGame
 	);
 };
+
+function initGame(data, status) {
+	if (status === 'OK') {
+		guessingMap = new google.maps.Map(document.getElementById('guessingMap'), {
+			center: {
+				lat: 0,
+				lng: 0
+			},
+			zoom: 1,
+			disableDefaultUI: true,
+			fullscreenControl: true,
+			zoomControl: true,
+			zoomControlOptions: {
+				style: google.maps.ZoomControlStyle.LARGE 
+			},
+		});
+
+		var marker = new google.maps.Marker({
+			map: guessingMap,
+			position: { lat: 0, lng: 0 },
+			draggable: true,
+			title: 'Guessing Marker'
+		});
+
+		google.maps.event.addListener(marker, 'dragend', function() {
+			updateMarker();
+		});
+
+		guessingMap.addListener('click', (mapsMouseEvent) => {
+			marker.setPosition(mapsMouseEvent.latLng);
+			updateMarker();
+		});
+
+		function updateMarker() {
+			markerLat = marker.getPosition().lat();
+			markerLong = marker.getPosition().lng();
+			document.getElementById('marker').innerHTML = 'Marker At: ' + markerLat + ',' + markerLong;
+			console.log(markerLat + ',' + markerLong);
+		}
+
+		panorama = new google.maps.StreetViewPanorama(document.getElementById('streetView'));
+		panorama.setOptions({
+			addressControl: false,
+			showRoadLabels: false
+		});
+
+		panorama.setPano(data.location.pano);
+		panorama.setPov({
+			heading: 270,
+			pitch: 0
+		});
+		panorama.setVisible(true);
+	} else {
+		console.log('Street View not found.');
+		window.initMap();
+	}
+}
 
 function rangeRandom(min, max) {
 	return Math.random() * (max - min) + min;
